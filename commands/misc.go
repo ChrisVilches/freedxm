d6bd52b55e35d2bfa8ebe9d9dcbfe429d53ebcfc
Chris Vilches <felovilches@gmail.com>
2025-05-04 22:39:39 +0900

    improve output of session list command 'freedxm ls'

--- commands/misc.go
+++ commands/misc.go
@@ -3,18 +3,30 @@ package commands
 import (
 	"context"
 	"fmt"
+	"time"
+
 	rpc "github.com/ChrisVilches/freedxm/rpc/implementation"
 	"github.com/urfave/cli/v3"
 )
 
 func ListSessions(_ context.Context, cmd *cli.Command) error {
 	port := int(cmd.Int("port"))
-	sessions, err := rpc.ListSessions(port)
+	sessionList, err := rpc.ListSessions(port)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(sessions)
+	for _, session := range sessionList.Sessions {
+		createdAt := session.CreatedAt.AsTime()
+		diff := time.Now().Sub(createdAt)
+		diffSeconds := int(diff.Seconds())
+		format := "2006-01-02 15:04:05"
+
+		fmt.Printf("Created at: %v\n", createdAt.In(time.Local).Format(format))
+		fmt.Printf("%d/%ds\n", diffSeconds, session.TimeSeconds)
+		fmt.Println(session.BlockLists)
+	}
+
 	return nil
 }
 
