c47bd2c009231a6f8f9de85acd9fb56a1223f3a8
Chris Vilches <felovilches@gmail.com>
2025-05-17 00:06:59 +0900

    improve freedxm ls

--- commands/misc.go
+++ commands/misc.go
@@ -6,9 +6,34 @@ import (
 	"time"
 
 	rpc "github.com/ChrisVilches/freedxm/rpc/implementation"
+	"github.com/ChrisVilches/freedxm/rpc/pb"
 	"github.com/ChrisVilches/freedxm/util"
 	"github.com/urfave/cli/v3"
 )
+
+func printBlockLists(blockLists []*pb.BlockList) {
+	for _, blockList := range blockLists {
+		fmt.Printf("  %s\n", blockList.Name)
+		for _, domain := range blockList.Domains {
+			fmt.Printf("    %s\n", domain)
+		}
+		for _, proc := range blockList.Processes {
+			fmt.Printf("    %s\n", proc)
+		}
+	}
+}
+
+func printSessionTimeInfo(session *pb.Session) {
+	createdAt := session.CreatedAt.AsTime()
+	diff := int32(time.Now().Sub(createdAt).Seconds())
+	format := "2006-01-02 15:04:05"
+	elapsed := util.SecondsToHHMMSS(diff)
+	totalFmt := util.SecondsToHHMMSS(session.TimeSeconds)
+
+	fmt.Printf("Since %v", createdAt.In(time.Local).Format(format))
+	percentage := float64(diff) * 100 / float64(session.TimeSeconds)
+	fmt.Printf(" (%.2f%%, %s / %s)\n", percentage, elapsed, totalFmt)
+}
 
 func ListSessions(_ context.Context, cmd *cli.Command) error {
 	port := int(cmd.Int("port"))
@@ -22,16 +47,8 @@ func ListSessions(_ context.Context, cmd *cli.Command) error {
 			fmt.Println()
 		}
 
-		createdAt := session.CreatedAt.AsTime()
-		diff := int32(time.Now().Sub(createdAt).Seconds())
-		format := "2006-01-02 15:04:05"
-		elapsed := util.SecondsToHHMMSS(diff)
-		totalFmt := util.SecondsToHHMMSS(session.TimeSeconds)
-
-		fmt.Printf("Since %v", createdAt.In(time.Local).Format(format))
-		percentage := float64(diff) * 100 / float64(session.TimeSeconds)
-		fmt.Printf(" (%.2f%%, %s / %s)\n", percentage, elapsed, totalFmt)
-		fmt.Println(session.BlockLists)
+		printSessionTimeInfo(session)
+		printBlockLists(session.BlockLists)
 	}
 
 	return nil
